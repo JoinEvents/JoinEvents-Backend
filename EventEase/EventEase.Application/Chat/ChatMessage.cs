6185219eabefdf5131b3d6ca1a3c0c2d468809d1
Chiranjeevi A <chiranjeeviaratakatla@gmail.com>
2026-05-16 13:50:44 +0530

    Fix nullable reference type warnings causing build failure

--- EventEase/EventEase.Application/Chat/ChatMessage.cs
+++ EventEase/EventEase.Application/Chat/ChatMessage.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +11,7 @@ namespace EventEase.Application.Chat
         public Guid Id { get; set; }
         public Guid SenderId { get; set; }
         public Guid ReceiverId { get; set; }
-        public string Message { get; set; }
+        public string Message { get; set; } = string.Empty;
         public DateTime Timestamp { get; set; } = DateTime.UtcNow;
     }
 }
